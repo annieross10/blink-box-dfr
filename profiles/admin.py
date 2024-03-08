7e6167247741d0e7329b83fbddc044713f180786
annieross10 <annieross10@outlookcom>
2024-03-08 15:55:51 +1000

    add profile views and serializers

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'profile_picture', 'bio', 'birth_date']
+
+admin.site.register(Profile, ProfileAdmin)
--- profiles/models.py
+++ profiles/models.py
@@ -2,11 +2,10 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import UserProfile
 
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='images/', default='../default_profile_qohjd5')
     bio = models.TextField(blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
@@ -16,4 +15,4 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        Profile.objects.create(user=instance)
