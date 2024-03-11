44788fb0d4a504c96503dfcb742e37975820353f
annieross10 <annieross10@outlookcom>
2024-03-11 13:18:12 +1000

    add model and views for like

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response 
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(APIView):
+    def post(self, request):
+        serializer = LikeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LikeDetail(APIView):
+    def delete(self, request, pk):
+        like = Like.objects.get(pk=pk)
+        like.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
