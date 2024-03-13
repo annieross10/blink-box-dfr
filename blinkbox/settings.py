3603577b0d20243cb4b3a4a8a59e8a42111ca6d8
annieross10 <annieross10@outlookcom>
2024-03-13 15:53:05 +1000

    update settings

--- blinkbox/settings.py
+++ blinkbox/settings.py
@@ -50,7 +50,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'blink-box-dfr.herokuapp.com', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', 'blink-box-dfr.herokuapp.com', '127.0.0.1', 'blink-box-drf-1e212f83bc7c.herokuapp.com']
 
 
 # Application definition
