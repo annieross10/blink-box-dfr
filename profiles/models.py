6a5a5d98bf3b14d3555e2fa5444bb9765cb76a49
annieross10 <annieross10@outlookcom>
2024-03-14 12:35:49 +1000

    migrate changes

--- profiles/models.py
+++ profiles/models.py
@@ -16,3 +16,5 @@ class Profile(models.Model):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+post_save.connect(create_profile, sender=User)
