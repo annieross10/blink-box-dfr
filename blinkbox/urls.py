8b602d585d0df4312a8c96e88dba237094f3dc74
annieross10 <annieross10@outlookcom>
2024-03-14 13:28:37 +1000

    update requirements.txt

--- blinkbox/urls.py
+++ blinkbox/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
-    ),    path('', root_route),
+    path( 'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),   
+    path('', root_route),
     path('profiles/', include('profiles.urls')), 
     path('posts/', include('posts.urls')),       
     path('comments/', include('comments.urls')),  
--- posts/serializers.py
+++ posts/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from posts.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    usuer = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Post
-        fields = ['id', 'user', 'content', 'created_at']
+        fields = ['id', 'user', 'created_at']
